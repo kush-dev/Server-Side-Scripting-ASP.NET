02a99969e0a79a071b1531b71b5f9d51c56af834
Allan-Dev <60462266+kush-dev@users.noreply.github.com>
2023-08-16 06:05:29 +0300

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context) => _context = context;
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products.ToListAsync();
+            return View(products);
+        }
+
+    }
+}
